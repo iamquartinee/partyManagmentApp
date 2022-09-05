280fb833aa7a8dae219f32d34dcc89f66cce36c1
Robert <15r.gaglewski@gmail.com>
2022-09-05 19:14:33 +0200

    Utworzenie prostego switcha oraz klasy Party

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dupa");
+        boolean stopApp = true;
+        Scanner scanner = new Scanner(System.in);
+        while(stopApp) {
+            System.out.println("Choose option from menu and press Enter.");
+            System.out.println("1. Add guest.");
+            System.out.println("2. Display guests.");
+            int userMenuChoice = scanner.nextInt();
+            switch (userMenuChoice) {
+                case 1 -> System.out.println("");
+                case 2 -> System.out.println("");
+
+
+            }
+        }
+
     }
 }
