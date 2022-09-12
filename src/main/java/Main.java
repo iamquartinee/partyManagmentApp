0da41077c60430d02338bd643986956270391e9e
Robert <15r.gaglewski@gmail.com>
2022-09-12 10:41:02 +0200

    Kolejne pracę nad projektem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,33 @@ public class Main {
         boolean stopApp = true;
         Scanner scanner = new Scanner(System.in);
         Party party = new Party();
+        int userMenuChoice;
         while (stopApp) {
             System.out.println("Choose option from menu and press Enter.");
             System.out.println("1. Add guest.");
             System.out.println("2. Display guests.");
-            int userMenuChoice = scanner.nextInt();
-            switch (userMenuChoice) {
-                case 1 -> party.addGuest();
-                case 2 -> party.displayGuests();
-
-
+            System.out.println("3. Display meals.");
+            System.out.println("4. Find guest by their phone number.");
+            System.out.println("5. Display vegans.");
+            System.out.println("6. Display non-vegans.");
+            System.out.println("7. Exit.");
+            try {
+                userMenuChoice = scanner.nextInt();
+                switch (userMenuChoice) {
+                    case 1 -> party.addGuest();
+                    case 2 -> party.displayGuests();
+                    case 3 -> party.displayMeals();
+                    case 4 -> party.findGuestByTheirPhoneNumber();
+                    case 5 -> party.displayVegans();
+                    case 6 -> party.displayNoVegans();
+                    case 7 -> stopApp=false;
+                }
+            } catch (Exception e) {
+                System.out.println("Something went wrong ;c");
+                scanner.next();
             }
+
+
         }
 
     }
--- src/main/java/Party.java
+++ src/main/java/Party.java
@@ -1,11 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Party {
     List<Guest> guests = new ArrayList();
+    List<String> vegans = new ArrayList<>();
+    List<String> noVegans = new ArrayList<>();
     private final Scanner scanner = new Scanner(System.in);
-
+    Set<String> meals = new HashSet<>();
+    Map<String, String> guestsPhoneNumbers = new HashMap<>();
     void addGuest() {
         System.out.println("What's your guest's name?");
         String name = scanner.nextLine();
@@ -16,13 +17,42 @@ public class Party {
         System.out.println("Is your guest vegan? Y-yes | N-No");
         Boolean isVegan = scanner.nextLine().equalsIgnoreCase("Y");
         Guest guest = new Guest(name, meal, phoneNumber, isVegan);
+        meals.add(meal);
+        guestsPhoneNumbers.put(phoneNumber, name);
+        System.out.println("Isvegan="+isVegan);
+        isVegan = true ? vegans.add(name) : noVegans.add(name);
         guests.add(guest);
+
     }
 
     void displayGuests() {
         for (Guest guest : guests) {
             guest.displayGuestsInfo();
             System.out.println();
+        }
+    }
+    void displayMeals() {
+        for (String meal : meals) {
+            System.out.println(meal);
+        }
+    }
+    void findGuestByTheirPhoneNumber() {
+        System.out.println("What's your missing guest phone number?");
+        String phoneNumber = scanner.nextLine();
+        for (Guest guest : guests) {
+            if(guest.getPhoneNumber().equals(phoneNumber)) { guest.displayGuestsInfo();}
+        }
+    }
+    void displayVegans() {
+        for (String vegan : vegans) {
+            System.out.println(vegan);
+            System.out.println("");
+        }
+    }
+    void displayNoVegans() {
+        for (String noVegan : noVegans) {
+            System.out.println(noVegan);
+            System.out.println("");
         }
     }
 
